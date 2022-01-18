16d10667c02e6d2281c3b9fbafaf9d75a7c7f74f
Vova Truskov <truskov-vova3@mail.ru>
2022-01-18 17:11:53 +0300

    fixes

--- Assets/AzurGames/_Player/PlayerController.cs
+++ Assets/AzurGames/_Player/PlayerController.cs
@@ -12,7 +12,7 @@ namespace AzurGames.Player
         
         private static readonly int IsRunning = Animator.StringToHash("isRunning");
         private bool _canMove = true;
-        private float positionX;
+        private float _positionX;
 
         private GameSettings _gameSettings;
 
@@ -56,11 +56,11 @@ namespace AzurGames.Player
         {
             playerTransform.position += Vector3.forward * Time.deltaTime * _gameSettings.PlayerForwardMovementSpeed;
 
-            positionX += Input.GetAxis("Mouse X") * Time.deltaTime * _gameSettings.PlayerSideMovementSpeed;
+            _positionX += Input.GetAxis("Mouse X") * Time.deltaTime * _gameSettings.PlayerSideMovementSpeed;
             
-            if(positionX > _gameSettings.PlayerSidePositionLimit || positionX < -_gameSettings.PlayerSidePositionLimit) return;
+            if(_positionX > _gameSettings.PlayerSidePositionLimit || _positionX < -_gameSettings.PlayerSidePositionLimit) return;
 
-            playerTransform.position = new Vector3(positionX, playerTransform.position.y, playerTransform.position.z);
+            playerTransform.position = new Vector3(_positionX, playerTransform.position.y, playerTransform.position.z);
         }
 
         private void StopMove()
@@ -81,7 +81,7 @@ namespace AzurGames.Player
         {
             playerTransform.position = Vector3.zero;
             _canMove = true;
-            positionX = 0;
+            _positionX = 0;
             GameManager.SetState(State.Start);
         }
     }
